66f96fcf0d3cd99d6e177c1ed0720fd4583e5bc0
KakosBoss <kyriakosgeorgiades@outlook.com>
2023-10-24 11:28:48 +0300

    Add project files.

---
+++ JennySales.App/Program.cs
@@ -0,0 +1,8 @@
+﻿using JennySales.Helpers;
+
+System.Console.WriteLine("Enter the year:");
+int year = int.Parse(System.Console.ReadLine());
+decimal standardDeviation = SalesStatistics.YearStandardDeviation(year);
+System.Console.WriteLine($"The standard deviation for the year {year} is: {standardDeviation:F2}");
+System.Console.WriteLine("\nPress Enter to close the application.");
+System.Console.ReadLine();
